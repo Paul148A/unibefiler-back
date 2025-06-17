5af7b0e568964ee961bd727517c3a000da6b5be0
JonaCaiza <jiy.caiza@yavirac.edu.ec>
2025-06-17 10:06:04 -0500

    preview PDF

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.useStaticAssets(join(__dirname, '..', 'uploads', 'documentos-personales'), {
+    prefix: '/documentos-personales/',
+  });
+  app.useStaticAssets(join(__dirname, '..', 'uploads', 'documentos-inscripcion'), {
+    prefix: '/documentos-inscripcion/',
+  });
+  app.useStaticAssets(join(__dirname, '..', 'uploads', 'documentos-grado'), {
+    prefix: '/documentos-grado/',
+  });
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
